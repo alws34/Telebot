33ce9837da6f66a4fa0f9ae59d028e184ab878e9
jdahan91 <jdahan91@users.noreply.github.com>
2020-03-29 00:29:13 +0300

    Update PrimitiveExtensions.cs

--- Telebot/Extensions/PrimitiveExtensions.cs
+++ Telebot/Extensions/PrimitiveExtensions.cs
@@ -4,7 +4,7 @@
     {
         public static string AsReadable(this bool value)
         {
-            return value ? "Active" : "Inactive";
+            return value ? "Active ✅" : "Inactive ❌";
         }
     }
 }
